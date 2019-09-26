ca60eee1a25e8c5999913d63f534b2a6286fb515
Vikas Chouhan <vikas.chouhan@valuelabs.com>
2019-09-26 15:13:54 +0530

    C51 done

---
+++ sources/app/assets/vue/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="row col">
+      <h1>Offers</h1>
+    </div>
+
+    <div class="row col">
+      <p>Offers and there cashbacks!!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
---
+++ sources/app/src/Controller/IndexController.php
@@ -0,0 +1,34 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Omines\DataTablesBundle\Adapter\ArrayAdapter;
+use Omines\DataTablesBundle\Column\TextColumn;
+use Omines\DataTablesBundle\Controller\DataTablesTrait;
+use Symfony\Component\HttpFoundation\Request;
+
+final class IndexController extends Controller
+{
+    
+    use DataTablesTrait;
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
+    public function indexAction(Request $request): Response
+    {
+        $offers = json_decode(file_get_contents("https://api.myjson.com/bins/lugxd"),true);
+        $offers = $offers['offers'];
+
+        //print_r($offers);die;
+
+        return $this->render('base.html.twig', [
+            'offers' => $offers,
+        ]);
+    }
+}
